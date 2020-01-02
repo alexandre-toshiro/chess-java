2ae201898f5fb42c27df44a985d42f9af21a6fda
Toshiro <toshiro.lyn@gmail.com>
2020-01-02 19:38:00 -0300

    Checkmate logic 2

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -80,7 +80,7 @@ public class ChessMatch {
 
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 
-		if (testCheck(opponent(currentPlayer))) {
+		if (testCheckMate(opponent(currentPlayer))) {
 			checkMate = true;
 		} else {
 			nextTurn();
